7f82f74c2d039781c7f8bddd6a47ca6687906930
Wesley Westelley <wesleywestelley@gmail.com>
2022-02-14 16:46:14 -0300

    :white_check_mark: chore: add favicon for application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,11 @@ function MyApp({ Component, pageProps }: AppProps) {
             <Head>
                 <title>MySpotify</title>
                 <link rel="manifest" href="manifest.json" />
+                <link
+                    rel="shortcut icon"
+                    href="/assets/spotify.svg"
+                    type="image/x-icon"
+                />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link
                     href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700&display=swap"
