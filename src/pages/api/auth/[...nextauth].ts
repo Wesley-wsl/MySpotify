fdf55b1070c19f3d48c106376498f6952fa32557
Wesley Westelley <wesleywestelley@gmail.com>
2022-02-17 11:18:26 -0300

    :white_check_mark: feat: create router for next-auth

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,25 @@
+import NextAuth from "next-auth";
+import SpotifyProvider from "next-auth/providers/spotify";
+
+export default NextAuth({
+    providers: [
+        SpotifyProvider({
+            clientId: process.env.SPOTIFY_CLIENT_ID as string,
+            clientSecret: process.env.SPOTIFY_CLIENT_SECRET as string,
+            authorization:
+                "https://accounts.spotify.com/authorize?scope=user-read-email,playlist-read-private",
+        }),
+    ],
+    callbacks: {
+        async jwt({ token, account }) {
+            if (account) {
+                token.accessToken = account.access_token;
+            }
+            return token;
+        },
+        async session({ session, token }) {
+            session.accessToken = token.accessToken;
+            return session;
+        },
+    },
+});
