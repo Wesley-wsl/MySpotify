d86f3b76bcc5f1890f10ae764eb3d750dcb865bf
Wesley Westelley <wesleywestelley@gmail.com>
2022-02-18 18:29:14 -0300

    :recycle: refactor: move fonts for _document.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,10 +16,6 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
                     type="image/x-icon"
                 />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700&display=swap"
-                    rel="stylesheet"
-                />
                 <meta name="description" content="Boilerplate Nextjs" />
                 <meta name="theme-color" content="#000" />
             </Head>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -37,7 +37,12 @@ export default class MyDocument extends Document {
     render() {
         return (
             <Html lang="en">
-                <Head />
+                <Head>
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700&display=swap"
+                        rel="stylesheet"
+                    />
+                </Head>
                 <body>
                     <Main />
                     <NextScript />
