64392e1f8ecfa1b385c7ca9c19b23309acf263fc
Wesley Westelley <wesleywestelley@gmail.com>
2022-02-24 16:58:50 -0300

    :recycle: refactor: fetch data from client side with SWR

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,22 +5,34 @@ import { IHome } from "../../@types";
 import List from "../../components/List";
 import Sidebar from "../../components/Sidebar";
 import Topbar from "../../components/Topbar";
-import { api } from "../../services/api";
+import { useFetch } from "../../hooks/useFetch";
 import * as S from "../../styles/pages/Home";
 
-export default function Home({ releases, recentlyPlayed }: IHome) {
+export default function Home({ accessToken }: IHome) {
+    const { data: releasess, error } = useFetch(
+        "browse/new-releases?limit=12",
+        `${accessToken}`,
+    );
+
+    const { data: recentlyPlayed, error: recentlyError } = useFetch(
+        "me/player/recently-played?limit=4",
+        `${accessToken}`,
+    );
+
+    if (error || recentlyError) console.log(error, recentlyError);
+    if (!releasess || !recentlyPlayed) return "Loading";
+
     return (
         <S.Container>
             <Sidebar />
             <Topbar />
-
             <List
-                recently={recentlyPlayed}
+                recently={recentlyPlayed.items}
                 type="Albums"
                 title="New releases"
             />
 
-            <List album={releases} type="Albums" title="Albums" />
+            <List album={releasess.albums.items} type="Albums" title="Albums" />
         </S.Container>
     );
 }
@@ -37,28 +49,9 @@ export const getServerSideProps: GetServerSideProps = async ctx => {
         };
     }
 
-    const recentlyPlayed = await api
-        .get("me/player/recently-played?limit=4", {
-            headers: {
-                Authorization: `Bearer ${session.accessToken}`,
-            },
-        })
-        .then(res => res.data.items)
-        .catch(error => console.log(error.message));
-
-    const releases = await api
-        .get("browse/new-releases?limit=12", {
-            headers: {
-                Authorization: `Bearer ${session.accessToken}`,
-            },
-        })
-        .then(res => res.data.albums.items)
-        .catch(error => console.log(error.message));
-
     return {
         props: {
-            releases,
-            recentlyPlayed,
+            accessToken: session.accessToken,
         },
     };
 };
