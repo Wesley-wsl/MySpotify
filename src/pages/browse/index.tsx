8ec744ea2c4ff669a66b552e3cea17dd4952012d
Wesley Westelley <wesleywestelley@gmail.com>
2022-02-24 17:10:00 -0300

    :white_check_mark: feat: create page browse

---
+++ src/pages/browse/index.tsx
@@ -0,0 +1,33 @@
+import { GetServerSideProps } from "next";
+import { getSession } from "next-auth/react";
+import React from "react";
+
+import Sidebar from "../../components/Sidebar";
+import Topbar from "../../components/Topbar";
+import * as S from "../../styles/pages/Browse";
+
+export default function Browse() {
+    return (
+        <S.Container>
+            <Sidebar />
+            <Topbar />
+        </S.Container>
+    );
+}
+
+export const getServerSideProps: GetServerSideProps = async ctx => {
+    const session = await getSession(ctx);
+
+    if (!session) {
+        return {
+            redirect: {
+                destination: "/",
+                permanent: false,
+            },
+        };
+    }
+
+    return {
+        props: {},
+    };
+};
