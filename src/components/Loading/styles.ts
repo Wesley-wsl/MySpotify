84881226d68d9723c9ea8b7662f79ad97255cbab
Wesley Westelley <wesleywestelley@gmail.com>
2022-02-24 20:58:51 -0300

    :art: style: centralize Loading and add animations

---
+++ src/components/Loading/styles.ts
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const Loader = styled.div`
+    display: inline-block;
+    width: 80px;
+    height: 80px;
+
+    &:after {
+        content: " ";
+        display: block;
+        width: 64px;
+        height: 64px;
+        margin: 8px;
+        border-radius: 50%;
+        border: 6px solid #f1f1f1;
+        border-color: #f1f1f1 transparent #f1f1f1 transparent;
+        animation: loadingEffect 1.2s linear infinite;
+    }
+    @keyframes loadingEffect {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+`;
+
+export const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
