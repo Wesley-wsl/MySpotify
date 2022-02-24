f43be99beabf610e1d47e3d166cf4364496c279d
Wesley Westelley <wesleywestelley@gmail.com>
2022-02-24 17:10:58 -0300

    :art: style: default configs for main

---
+++ src/styles/pages/Browse.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+    background-color: var(--dark-secoundary);
+    color: var(--white-secoundary);
+    padding-left: 34rem;
+    min-height: 100vh;
+`;
