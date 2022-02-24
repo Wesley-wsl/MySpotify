67ac659c639c06be9f24f19eb6fa89f8b15d74d6
Wesley Westelley <wesleywestelley@gmail.com>
2022-02-24 20:58:03 -0300

    :wrench: feat: create loading component

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import * as S from "./styles";
+
+const Loading: React.FC = () => {
+    return (
+        <S.Container>
+            <S.Loader />;
+        </S.Container>
+    );
+};
+
+export default Loading;
