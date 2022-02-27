05dc349ff95369f1d4c3f1a66a8c40dac680e9bb
Wesley Westelley <wesleywestelley@gmail.com>
2022-02-27 16:00:57 -0300

    :art: style: create responsive

--- src/components/Sidebar/styles.ts
+++ src/components/Sidebar/styles.ts
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
+import { ISideBar } from "../../@types";
+
 export const Container = styled.div`
     background-color: var(--dark-primary);
     width: 24rem;
@@ -11,7 +13,8 @@ export const Container = styled.div`
     top: 0;
     left: 0;
     padding-top: 2rem;
-
+    transition: transform 0.1s linear;
+    z-index: 999;
     nav {
         padding-top: 1.4rem;
         padding-bottom: 1.3rem;
@@ -33,6 +36,11 @@ export const Container = styled.div`
     svg {
         width: 2.5rem;
         height: 2.5rem;
+    }
+
+    @media (max-width: 810px) {
+        transform: ${({ isOpen }: ISideBar) =>
+            isOpen ? "translateX(0)" : "translateX(-100%)"};
     }
 `;
 
@@ -80,4 +88,49 @@ export const Label = styled.p`
     color: var(--grey-secoundary);
     margin: 0.3rem 2rem;
     font-weight: 200;
+`;
+
+export const Mobile = styled.div`
+    position: fixed;
+    top: 0rem;
+    bottom: 0;
+    left: 0rem;
+    padding-top: 1rem;
+    background-color: #000;
+    width: 5.6rem;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-radius: 0.5rem;
+    z-index: 100;
+    cursor: pointer;
+
+    div {
+        width: 2.5rem;
+        height: 0.2rem;
+        background-color: #f1f1f1;
+        margin-bottom: 0.5rem;
+    }
+`;
+
+export const Close = styled.div`
+    font-size: 2rem;
+    padding: 0.5rem;
+    width: 90%;
+    background-color: var(--dark-green-secoundary);
+    color: #f1f1f1;
+    border-radius: 2rem;
+    font-weight: 400;
+    text-align: center;
+    cursor: pointer;
+    transition: all 0.2s linear;
+    &:hover {
+        transform: scale(110%);
+    }
+
+    @media (min-width: 810px) {
+        display: none;
+    }
 `;
