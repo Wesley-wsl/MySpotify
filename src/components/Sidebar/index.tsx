ee63fe1e881c3679a307433c4f1d7f77f8749707
Wesley Westelley <wesleywestelley@gmail.com>
2022-02-27 16:00:29 -0300

    :wrench: feat: create mobile

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { useState } from "react";
 
 import BrowseSvg from "../../assets/icons/browse.svg";
 import HomeSvg from "../../assets/icons/home.svg";
@@ -6,66 +7,82 @@ import LikedSongsSvg from "../../assets/icons/liked-songs.svg";
 import * as S from "./styles";
 
 const Sidebar: React.FC = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <S.Container>
-            <S.MainNavigation>
-                <ul>
-                    <li>
-                        <Link href="/home">
-                            <a>
-                                <HomeSvg aria-label="Home icon" />
-                                Home
-                            </a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/browse">
-                            <a>
-                                <BrowseSvg aria-label="browse icon" />
-                                Browse
-                            </a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/liked-songs">
-                            <a>
-                                <LikedSongsSvg aria-label="heart icon" />
-                                Liked songs
-                            </a>
-                        </Link>
-                    </li>
-                </ul>
-            </S.MainNavigation>
+        <>
+            <S.Container isOpen={isOpen}>
+                {isOpen && (
+                    <S.Close onClick={() => setIsOpen(!isOpen)}>X</S.Close>
+                )}
 
-            <S.Label>Your Library</S.Label>
+                <S.MainNavigation>
+                    <ul>
+                        <li>
+                            <Link href="/home">
+                                <a>
+                                    <HomeSvg aria-label="Home icon" />
+                                    Home
+                                </a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/browse">
+                                <a>
+                                    <BrowseSvg aria-label="browse icon" />
+                                    Browse
+                                </a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/liked-songs">
+                                <a>
+                                    <LikedSongsSvg aria-label="heart icon" />
+                                    Liked songs
+                                </a>
+                            </Link>
+                        </li>
+                    </ul>
+                </S.MainNavigation>
 
-            <nav>
-                <S.ListStyle>
-                    <li>
-                        <Link href="/recent-played">Recent played</Link>
-                    </li>
-                    <li>
-                        <Link href="/albums">Albums</Link>
-                    </li>
-                    <li>
-                        <Link href="/artists">Artists</Link>
-                    </li>
-                </S.ListStyle>
-            </nav>
+                <S.Label>Your Library</S.Label>
 
-            <S.Label>Playlists</S.Label>
+                <nav>
+                    <S.ListStyle>
+                        <li>
+                            <Link href="/recent-played">Recent played</Link>
+                        </li>
+                        <li>
+                            <Link href="/albums">Albums</Link>
+                        </li>
+                        <li>
+                            <Link href="/artists">Artists</Link>
+                        </li>
+                    </S.ListStyle>
+                </nav>
 
-            <nav>
-                <S.ListStyle>
-                    <li>
-                        <Link href="/chill">Chill</Link>
-                    </li>
-                    <li>
-                        <Link href="/lofi">Lofi</Link>
-                    </li>
-                </S.ListStyle>
-            </nav>
-        </S.Container>
+                <S.Label>Playlists</S.Label>
+
+                <nav>
+                    <S.ListStyle>
+                        <li>
+                            <Link href="/chill">Chill</Link>
+                        </li>
+                        <li>
+                            <Link href="/lofi">Lofi</Link>
+                        </li>
+                    </S.ListStyle>
+                </nav>
+            </S.Container>
+
+            {!isOpen && (
+                <S.Mobile onClick={() => setIsOpen(!isOpen)}>
+                    <div />
+                    <div />
+                    <div />
+                </S.Mobile>
+            )}
+        </>
     );
 };
 
