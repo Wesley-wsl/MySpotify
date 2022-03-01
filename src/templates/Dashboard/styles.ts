bb4a452a8f7863baf9a0c8d3960eced1212ad579
Wesley Westelley <wesleywestelley@gmail.com>
2022-03-01 13:39:53 -0300

    :art: style: create style for template dashboard

---
+++ src/templates/Dashboard/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+    background-color: var(--dark-secoundary);
+    color: var(--white-secoundary);
+    padding-left: 30rem;
+    min-height: 100vh;
+
+    @media (max-width: 810px) {
+        padding-left: 7.5rem;
+    }
+`;
