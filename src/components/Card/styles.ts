3fa5cab0438f6fdb7b9687b75a5d1247f7bd85b2
Wesley Westelley <wesleywestelley@gmail.com>
2022-03-02 14:06:39 -0300

    :art: style: add style for Card component

---
+++ src/components/Card/styles.ts
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    cursor: pointer;
+    margin-bottom: 4rem;
+    transition: all 0.2s linear;
+    max-width: 20rem;
+    &:hover {
+        transform: scale(120%);
+    }
+
+    .border_radius {
+        border-radius: 50%;
+    }
+
+    img {
+        border-radius: 0.5rem;
+        filter: contrast(120%);
+    }
+
+    p {
+        text-align: center;
+        max-width: 20rem;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
+
+    .FakeImage {
+        width: 20rem;
+        height: 20rem;
+        border-radius: 0.5rem;
+        filter: contrast(120%);
+        background-color: var(--dark-primary);
+    }
+
+    .FakeImageBorder {
+        width: 20rem;
+        height: 20rem;
+        filter: contrast(120%);
+        background-color: var(--dark-primary);
+        border-radius: 50%;
+    }
+`;
+
+export const Title = styled.p`
+    font-size: 2rem;
+    margin-top: 0.5rem;
+`;
+
+export const Author = styled.p`
+    font-size: 1.6rem;
+    color: var(--grey-primary);
+`;
+
+export const TitleArtists = styled.p`
+    font-size: 2rem;
+    margin: 0 auto;
+    margin-top: 0.5rem;
+    text-align: center;
+`;
