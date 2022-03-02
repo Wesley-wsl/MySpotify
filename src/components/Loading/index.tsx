e1b575a1c66a79738232429458285875ebf1fcc6
Wesley Westelley <wesleywestelley@gmail.com>
2022-03-02 20:25:50 -0300

    :test_tube: chore: add attribute for tests

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -5,7 +5,7 @@ import * as S from "./styles";
 const Loading: React.FC = () => {
     return (
         <S.Container>
-            <S.Loader />
+            <S.Loader data-testid="Loading" />
         </S.Container>
     );
 };
