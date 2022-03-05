c1836b6e9ee68813b70d0243bde115a0ec82b9c2
Wesley Westelley <wesleywestelley@gmail.com>
2022-03-05 14:40:34 -0300

    :wrench: chore: allow style files in jest

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,8 @@ module.exports = {
     testPathIgnorePatterns: ["/node_modules/", "/.next/"],
     collectCoverage: true,
     collectCoverageFrom: ["src/**/*.test.ts(x)"],
-    setupFilesAfterEnv: ["<rootDir>/.jest/setup.ts"]
+    setupFilesAfterEnv: ["<rootDir>/.jest/setup.ts"],
+    moduleNameMapper: {
+        "\\.(scss|css|sass)$": "identity-obj-proxy"
+    }
 }
