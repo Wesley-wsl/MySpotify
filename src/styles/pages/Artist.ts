dc702f5edbdc747529c53615c32a1bf8ae53f71d
Wesley Westelley <wesleywestelley@gmail.com>
2022-03-05 14:46:03 -0300

    :art: style: create style for Artist page

---
+++ src/styles/pages/Artist.ts
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+    background-color: var(--dark-secoundary);
+    color: var(--white-secoundary);
+    padding-left: 24rem;
+    padding-bottom: 5rem;
+    min-height: 100vh;
+
+    @media (max-width: 810px) {
+        padding-left: 0;
+    }
+`;
+
+export const List = styled.section`
+    padding-left: 10rem;
+    h2 {
+        font-weight: 500;
+        margin-top: 4.5rem;
+        margin-bottom: 3rem;
+        font-size: 3.2rem;
+        letter-spacing: 0.25rem;
+    }
+`;
+
+export const TopTracks = styled.section`
+    h2 {
+        padding-left: 10rem;
+        font-weight: 500;
+        margin-top: 4.5rem;
+        margin-bottom: 3rem;
+        font-size: 3.2rem;
+        letter-spacing: 0.25rem;
+    }
+`;
