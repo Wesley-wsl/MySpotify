952905000afe4304f82f177505ed9cc8dea966a2
Wesley Westelley <wesleywestelley@gmail.com>
2022-03-05 14:46:40 -0300

    :art: style: add padding-bottom

--- src/templates/Dashboard/styles.ts
+++ src/templates/Dashboard/styles.ts
@@ -5,6 +5,7 @@ export const Container = styled.main`
     color: var(--white-secoundary);
     padding-left: 30rem;
     min-height: 100vh;
+    padding-bottom: 5rem;
 
     @media (max-width: 810px) {
         padding-left: 7.5rem;
