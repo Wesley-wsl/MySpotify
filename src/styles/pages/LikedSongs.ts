031cc90ed2bc2b601f17882dd9d4fa22d0e36411
Wesley Westelley <wesleywestelley@gmail.com>
2022-03-05 14:45:38 -0300

    :art: style: create style for LikedSongs page

---
+++ src/styles/pages/LikedSongs.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+    margin: 5rem 0 3rem 0;
+    font-size: 3.2rem;
+`;
+
+export const LikedSongs = styled.div`
+    margin-left: -10rem;
+    table {
+        width: 84%;
+        margin-right: 0;
+    }
+`;
