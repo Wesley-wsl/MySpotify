fac2e2ed24740aa4fd957f42ef00a562a738fa88
Wesley Westelley <wesleywestelley@gmail.com>
2022-03-05 22:07:57 -0300

    :art: style: list colors for theme dark/light

---
+++ src/styles/theme/index.ts
@@ -0,0 +1,25 @@
+const dark = {
+    primary: "#020202",
+    secondary: "#040404",
+    thirdy: "#121212",
+    sideBar: "#000",
+    color: "#fff",
+    buttonLogin: "#1DB954",
+    buttonHover: "#1DB95415",
+    greyPrimary: "#646464",
+    greySecoundary: "#c4c4c4",
+};
+
+const light = {
+    primary: "rgb(230, 230, 230)",
+    secondary: "rgb(250, 250, 250)",
+    sideBar: "rgb(245, 240, 240)",
+    thirdy: "#fff",
+    color: "#000",
+    buttonLogin: "#1DB954",
+    buttonHover: "#1DB954aa",
+    greyPrimary: "#646464",
+    greySecoundary: "#c4c4c4",
+};
+
+export { dark, light };
