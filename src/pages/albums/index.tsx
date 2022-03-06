fb24fb401cb22c069006254853138d19c28c087b
Wesley Westelley <wesleywestelley@gmail.com>
2022-03-06 00:15:24 -0300

    :wrench: feat: create page for see albums

---
+++ src/pages/albums/index.tsx
@@ -0,0 +1,55 @@
+import { GetServerSideProps } from "next";
+import { getSession } from "next-auth/react";
+import { destroyCookie } from "nookies";
+
+import { IPageProps } from "../../@types";
+import List from "../../components/List";
+import Loading from "../../components/Loading";
+import { useFetch } from "../../hooks/useFetch";
+import * as S from "../../styles/shared";
+import DashboardTemplate from "../../templates/Dashboard";
+import { testToken } from "../../utils/testToken";
+
+export default function Albums({ accessToken }: IPageProps) {
+    const { data, error } = useFetch("me/albums", `${accessToken}`);
+
+    if (error) console.log(error);
+
+    return (
+        <DashboardTemplate>
+            {!data && <Loading />}
+
+            {data && data?.items.length !== 0 ? (
+                <List
+                    myAlbum={data.items}
+                    type="MyAlbums"
+                    title="Albums That You Saved"
+                />
+            ) : (
+                <S.NotExists>You don&apos;t have albums saved.</S.NotExists>
+            )}
+        </DashboardTemplate>
+    );
+}
+
+export const getServerSideProps: GetServerSideProps = async ctx => {
+    const session = await getSession(ctx);
+    const isValid = await testToken(`${session?.accessToken}`);
+
+    if (!session || !isValid) {
+        destroyCookie(ctx, "next-auth.session-token");
+
+        return {
+            redirect: {
+                destination: "/",
+                permanent: false,
+            },
+        };
+    }
+
+    return {
+        props: {
+            accessToken: session.accessToken,
+        },
+    };
+};
