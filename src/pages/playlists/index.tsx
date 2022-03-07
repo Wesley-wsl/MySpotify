92ba76d5fba1c1497e637b154c2c4395477f3134
Wesley Westelley <wesleywestelley@gmail.com>
2022-03-07 00:07:38 -0300

    :wrench: feat: create playlists pages
    this page show your playlists

---
+++ src/pages/playlists/index.tsx
@@ -0,0 +1,46 @@
+import { GetServerSideProps } from "next";
+import { getSession } from "next-auth/react";
+import { destroyCookie } from "nookies";
+import React from "react";
+
+import { IPageProps } from "../../@types";
+import List from "../../components/List";
+import { useFetch } from "../../hooks/useFetch";
+import DashboardTemplate from "../../templates/Dashboard";
+import { testToken } from "../../utils/testToken";
+
+export default function Playlists({ accessToken }: IPageProps) {
+    const { data, error } = useFetch("me/playlists", `${accessToken}`);
+
+    if (error) console.log(error);
+
+    return (
+        <DashboardTemplate>
+            {data && (
+                <List album={data.items} type="Playlists" title="Playlists" />
+            )}
+        </DashboardTemplate>
+    );
+}
+
+export const getServerSideProps: GetServerSideProps = async ctx => {
+    const session = await getSession(ctx);
+    const isValid = await testToken(`${session?.accessToken}`);
+
+    if (!session || !isValid) {
+        destroyCookie(ctx, "next-auth.session-token");
+
+        return {
+            redirect: {
+                destination: "/",
+                permanent: false,
+            },
+        };
+    }
+
+    return {
+        props: {
+            accessToken: session.accessToken,
+        },
+    };
+};
