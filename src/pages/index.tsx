9f75f7ea1a35ca9127ed62e9d00db93580cbeb1d
Wesley Westelley <wesleywestelley@gmail.com>
2022-03-10 23:18:16 -0300

    :lock: fix: resolve excess redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import { GetServerSideProps } from "next";
 import { signIn, getSession } from "next-auth/react";
+import { destroyCookie } from "nookies";
 
 import * as S from "../styles/pages/Login";
+import { testToken } from "../utils/testToken";
 
 export default function Login() {
     return (
@@ -17,6 +19,11 @@ export default function Login() {
 
 export const getServerSideProps: GetServerSideProps = async ctx => {
     const session = await getSession(ctx);
+    const isValid = await testToken(`${session?.accessToken}`);
+
+    if (!isValid) {
+        destroyCookie(ctx, "next-auth.session-token");
+    }
 
     if (session) {
         return {
