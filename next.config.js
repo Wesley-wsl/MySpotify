85329578f9993ef59c80c5d9d6c10c8a4cf49657
Wesley Westelley <wesleywestelley@gmail.com>
2022-06-20 01:28:38 -0300

    :white_check_mark: feat: add pwa

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
-module.exports = {
+const withPWA = require('next-pwa')
+const runtimeCaching = require("next-pwa/cache")
+
+module.exports = withPWA({
+    reactStrictMode: true,
     images: {
         domains: ["i.scdn.co", "mosaic.scdn.co", "seeded-session-images.scdn.co", "seed-mix-image.spotifycdn.com"],
     },
-};
+    pwa: {
+        dest: "public",
+        register: true,
+        skipWaiting: true,
+        runtimeCaching,
+        dynamicStartUrl: false,
+        buildExcludes: [/middleware-manifest.json$/]
+    }
+},)
