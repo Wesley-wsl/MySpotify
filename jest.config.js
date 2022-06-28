4136574ccf874437dec635325ca6435ab00addfc
Wesley Westelley <wesleywestelley@gmail.com>
2022-06-28 18:50:54 -0300

    :wrench: chore: configure coverage for tests

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
     testEnvironment: 'jsdom',
     testPathIgnorePatterns: ["/node_modules/", "/.next/"],
     collectCoverage: true,
-    collectCoverageFrom: ["src/**/*.test.ts(x)"],
+    collectCoverageFrom: ["<rootDir>/src/(components|pages)/**/*.ts(x)", "!**/_app.tsx", "!**/_document.tsx"],
     setupFilesAfterEnv: ["<rootDir>/.jest/setup.ts"],
     moduleNameMapper: {
         "\\.(scss|css|sass)$": "identity-obj-proxy"
