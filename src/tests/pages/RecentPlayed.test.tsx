92efe9b249e8d921855a1e65dccdec0a2e256de0
Wesley Westelley <wesleywestelley@gmail.com>
2022-06-29 16:59:54 -0300

    :test_tube: test: ensure that recent played is work correctly

--- src/tests/pages/RecentPlayed.test.tsx
+++ src/tests/pages/RecentPlayed.test.tsx
@@ -1,6 +1,9 @@
 import { screen, render } from "@testing-library/react";
+import { rest } from "msw";
+import { GetServerSidePropsContext } from "next";
 
-import RecentPlayed from "../pages/recent-played";
+import RecentPlayed, { getServerSideProps } from "../../pages/recent-played";
+import { baseURL, server } from "../mocks/setupServer";
 
 jest.mock("next-auth/react", () => {
     return {
@@ -8,7 +11,7 @@ jest.mock("next-auth/react", () => {
             return {
                 data: {
                     expires: "",
-                    accessToken: "",
+                    accessToken: "accessToken",
                     user: {
                         name: "Jorkis",
                         email: "jorkis@gmail.com",
@@ -17,17 +20,73 @@ jest.mock("next-auth/react", () => {
                 },
             };
         },
+        getSession: jest
+            .fn()
+            .mockResolvedValueOnce({ accessToken: "accessToken" })
+            .mockResolvedValueOnce(false),
+        signIn: jest.fn(),
+    };
+});
+
+jest.mock("../../utils/testToken", () => {
+    return {
+        testToken: jest
+            .fn()
+            .mockReturnValueOnce(true)
+            .mockReturnValueOnce(false),
     };
 });
 
 describe("RecentPlayed page", () => {
-    it("Should render RecentPlayed page", () => {
+    it("Shouldn't be able to render the list if data is not found.", () => {
+        server.use(
+            rest.get(`${baseURL}me/player/recently-played`, (req, res, ctx) => {
+                return res(ctx.status(400));
+            }),
+        );
+
         render(<RecentPlayed accessToken="accessToken" />);
 
-        const Navigation = screen.getByText("Home");
-        const Search = screen.getByPlaceholderText("Search");
+        const album = screen.queryByText("blackbear");
+        expect(album).not.toBeInTheDocument();
+    });
 
-        expect(Navigation).toBeInTheDocument();
-        expect(Search).toBeInTheDocument();
+    it("Should be able to render the list correctly", async () => {
+        render(<RecentPlayed accessToken="accessToken" />);
+
+        const recentlyPlayed = await screen.findByText("Recently Played");
+        const album = await screen.findByText("blackbear");
+
+        expect(recentlyPlayed).toBeInTheDocument();
+        expect(album).toBeInTheDocument();
+    });
+
+    it("getServerSideProps should be able to return a default props if user is authenticated.", async () => {
+        const ctx = {};
+
+        const response = await getServerSideProps(
+            ctx as GetServerSidePropsContext,
+        );
+
+        expect(response).toEqual({
+            props: {
+                accessToken: "accessToken",
+            },
+        });
+    });
+
+    it("getServerSideProps should be able to return a redirect props if user isn't authenticated.", async () => {
+        const ctx = {};
+
+        const response = await getServerSideProps(
+            ctx as GetServerSidePropsContext,
+        );
+
+        expect(response).toEqual({
+            redirect: {
+                destination: "/",
+                permanent: false,
+            },
+        });
     });
 });
