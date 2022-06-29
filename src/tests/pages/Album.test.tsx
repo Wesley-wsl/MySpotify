4dbdba4c471a0a981e975782d4a553bbeb6a055b
Wesley Westelley <wesleywestelley@gmail.com>
2022-06-29 20:58:26 -0300

    :test_tube: test: ensure that Album work correctly

--- src/tests/pages/Album.test.tsx
+++ src/tests/pages/Album.test.tsx
@@ -1,6 +1,9 @@
 import { screen, render } from "@testing-library/react";
+import { rest } from "msw";
+import { GetServerSidePropsContext } from "next";
 
-import Album from "../../pages/album/[id]";
+import Album, { getServerSideProps } from "../../pages/album/[id]";
+import { baseURL, server } from "../mocks/setupServer";
 
 jest.mock("next-auth/react", () => {
     return {
@@ -17,6 +20,20 @@ jest.mock("next-auth/react", () => {
                 },
             };
         },
+        getSession: jest
+            .fn()
+            .mockResolvedValueOnce({ accessToken: "accessToken" })
+            .mockResolvedValueOnce(false),
+        signIn: jest.fn(),
+    };
+});
+
+jest.mock("../../utils/testToken", () => {
+    return {
+        testToken: jest
+            .fn()
+            .mockReturnValueOnce(true)
+            .mockReturnValueOnce(false),
     };
 });
 
@@ -33,13 +50,58 @@ jest.mock("next/router", () => {
 });
 
 describe("Album page", () => {
-    it("Should render Album page", () => {
+    it("Shouldn't be able to render the album page with data if request was bad.", async () => {
+        server.use(
+            rest.get(`${baseURL}albums/*`, (req, res, ctx) => {
+                return res(ctx.status(400));
+            }),
+        );
+
         render(<Album accessToken="accessToken" />);
 
-        const Navigation = screen.getByText("Home");
-        const Search = screen.getByPlaceholderText("Search");
+        const albumImage = screen.queryByAltText("Album image");
+        const albumName = screen.queryByText("blackbear");
 
-        expect(Navigation).toBeInTheDocument();
-        expect(Search).toBeInTheDocument();
+        expect(albumImage).not.toBeInTheDocument();
+        expect(albumName).not.toBeInTheDocument();
+    });
+
+    it("Should render Album page with your informations.", async () => {
+        render(<Album accessToken="accessToken" />);
+
+        const albumImage = await screen.findByAltText("Album image");
+        const albumName = await screen.findByText("blackbear");
+
+        expect(albumImage).toBeInTheDocument();
+        expect(albumName).toBeInTheDocument();
+    });
+
+    it("getServerSideProps should be able to return a default props if user is authenticated.", async () => {
+        const ctx = {};
+
+        const response = await getServerSideProps(
+            ctx as GetServerSidePropsContext,
+        );
+
+        expect(response).toEqual({
+            props: {
+                accessToken: "accessToken",
+            },
+        });
+    });
+
+    it("getServerSideProps should be able to return a redirect props if user isn't authenticated.", async () => {
+        const ctx = {};
+
+        const response = await getServerSideProps(
+            ctx as GetServerSidePropsContext,
+        );
+
+        expect(response).toEqual({
+            redirect: {
+                destination: "/",
+                permanent: false,
+            },
+        });
     });
 });
