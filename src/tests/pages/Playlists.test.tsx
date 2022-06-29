6de6ca0f465f15257ee1e6126a771764faafe3cd
Wesley Westelley <wesleywestelley@gmail.com>
2022-06-29 17:37:04 -0300

    :test_tube: test: ensure that Playlists work correctly

--- src/tests/pages/Playlists.test.tsx
+++ src/tests/pages/Playlists.test.tsx
@@ -1,6 +1,9 @@
 import { screen, render } from "@testing-library/react";
+import { rest } from "msw";
+import { GetServerSidePropsContext } from "next";
 
-import Playlists from "../pages/playlists";
+import Playlists, { getServerSideProps } from "../../pages/playlists";
+import { baseURL, server } from "../mocks/setupServer";
 
 jest.mock("next-auth/react", () => {
     return {
@@ -17,17 +20,73 @@ jest.mock("next-auth/react", () => {
                 },
             };
         },
+        getSession: jest
+            .fn()
+            .mockResolvedValueOnce({ accessToken: "accessToken" })
+            .mockResolvedValueOnce(false),
+        signIn: jest.fn(),
+    };
+});
+
+jest.mock("../../utils/testToken", () => {
+    return {
+        testToken: jest
+            .fn()
+            .mockReturnValueOnce(true)
+            .mockReturnValueOnce(false),
     };
 });
 
 describe("Playlists page", () => {
-    it("Should render Playlists page", () => {
+    it("Shouldn't be able to render the list if data is not found.", () => {
+        server.use(
+            rest.get(`${baseURL}me/playlists`, (req, res, ctx) => {
+                return res(ctx.status(400));
+            }),
+        );
+
         render(<Playlists accessToken="accessToken" />);
 
-        const Navigation = screen.getByText("Home");
-        const Search = screen.getByPlaceholderText("Search");
+        const name = screen.queryByText("blackbear");
+        expect(name).not.toBeInTheDocument();
+    });
 
-        expect(Navigation).toBeInTheDocument();
-        expect(Search).toBeInTheDocument();
+    it("Should render Playlists page with a list of playlists.", async () => {
+        render(<Playlists accessToken="accessToken" />);
+
+        const playlists = await screen.findByText("Playlists");
+        const name = await screen.findByText("blackbear");
+
+        expect(playlists).toBeInTheDocument();
+        expect(name).toBeInTheDocument();
+    });
+
+    it("getServerSideProps should be able to return a default props if user is authenticated.", async () => {
+        const ctx = {};
+
+        const response = await getServerSideProps(
+            ctx as GetServerSidePropsContext,
+        );
+
+        expect(response).toEqual({
+            props: {
+                accessToken: "accessToken",
+            },
+        });
+    });
+
+    it("getServerSideProps should be able to return a redirect props if user isn't authenticated.", async () => {
+        const ctx = {};
+
+        const response = await getServerSideProps(
+            ctx as GetServerSidePropsContext,
+        );
+
+        expect(response).toEqual({
+            redirect: {
+                destination: "/",
+                permanent: false,
+            },
+        });
     });
 });
