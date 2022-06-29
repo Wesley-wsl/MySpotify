266119f36a9bd8d399b6213f8606278e2e5fad13
Wesley Westelley <wesleywestelley@gmail.com>
2022-06-29 00:20:35 -0300

    :test_tube: test: ensure all features in the top bar work

--- src/components/tests/Topbar.test.tsx
+++ src/components/tests/Topbar.test.tsx
@@ -1,7 +1,9 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import { signOut } from "next-auth/react";
 import Router from "next/router";
+import { Dispatch, SetStateAction } from "react";
 
+import { ThemeContext } from "../../contexts/Theme";
 import Topbar from "../Topbar";
 
 jest.mock("next-auth/react", () => {
@@ -24,6 +26,8 @@ jest.mock("next-auth/react", () => {
 });
 
 jest.mock("next/router");
+
+const setLightModeMocked = jest.fn() as Dispatch<SetStateAction<boolean>>;
 
 const signOutMocked = jest.mocked(signOut);
 const routerMocked = jest.mocked(Router);
@@ -74,6 +78,26 @@ describe("Topbar component", () => {
         expect(routerMocked.push).toHaveBeenCalled();
     });
 
+    it("Shouldn't be able to push to another router if don't have any value in search input.", () => {
+        render(<Topbar />);
+
+        const Search = screen.getByPlaceholderText(
+            "Search",
+        ) as HTMLInputElement;
+
+        fireEvent.change(Search, {
+            target: {
+                value: "",
+            },
+        });
+
+        expect(Search).toBeInTheDocument();
+        expect(Search.value).toEqual("");
+
+        fireEvent.click(screen.getByLabelText("Search icon"));
+        expect(routerMocked.push).not.toHaveBeenCalled();
+    });
+
     it("Should open option for log out on click in profile", () => {
         render(<Topbar />);
 
@@ -82,6 +106,71 @@ describe("Topbar component", () => {
         fireEvent.click(Profile);
 
         expect(screen.getByText("Log out")).toBeInTheDocument();
+    });
+
+    it("Should be able to close option for log out on click in overlay.", () => {
+        render(<Topbar />);
+
+        fireEvent.click(screen.getByText("Jorkis"));
+
+        const logout = screen.getByText("Log out");
+        expect(logout).toBeInTheDocument();
+
+        fireEvent.click(screen.getByTestId("overlay"));
+
+        expect(logout).not.toBeInTheDocument();
+    });
+
+    it("Should be able to active dark mode.", async () => {
+        render(
+            <ThemeContext.Provider
+                value={{ lightMode: true, setLightMode: setLightModeMocked }}
+            >
+                <Topbar />
+            </ThemeContext.Provider>,
+        );
+
+        const moon = screen.getByLabelText("Moon for change to dark mode.");
+        const sun = screen.queryByLabelText("Sun for change to light mode.");
+
+        expect(moon).toBeInTheDocument();
+        expect(sun).not.toBeInTheDocument();
+
+        fireEvent.click(moon);
+
+        waitFor(() => {
+            expect(
+                screen.queryByLabelText("Moon for change to dark mode."),
+            ).not.toBeInTheDocument();
+            expect(sun).toBeInTheDocument();
+            expect(setLightModeMocked).toHaveBeenCalled();
+        });
+    });
+
+    it("Should be able to active light mode.", async () => {
+        render(
+            <ThemeContext.Provider
+                value={{ lightMode: false, setLightMode: setLightModeMocked }}
+            >
+                <Topbar />
+            </ThemeContext.Provider>,
+        );
+
+        const moon = screen.queryByLabelText("Moon for change to dark mode.");
+        const sun = screen.getByLabelText("Sun for change to light mode.");
+
+        expect(moon).not.toBeInTheDocument();
+        expect(sun).toBeInTheDocument();
+
+        fireEvent.click(sun);
+
+        waitFor(() => {
+            expect(
+                screen.getByLabelText("Moon for change to dark mode."),
+            ).toBeInTheDocument();
+            expect(moon).toBeInTheDocument();
+            expect(setLightModeMocked).toHaveBeenCalled();
+        });
     });
 
     it("Should be able to log out", () => {
