5484f6df4739f43154d1e99233961cfdf5fe4b3b
Wesley Westelley <wesleywestelley@gmail.com>
2022-06-29 18:34:52 -0300

    :test_tube: test: ensure that LikedSongs work correctly

--- src/tests/pages/LikedSongs.test.tsx
+++ src/tests/pages/LikedSongs.test.tsx
@@ -1,6 +1,9 @@
 import { screen, render } from "@testing-library/react";
+import { rest } from "msw";
+import { GetServerSidePropsContext } from "next";
 
-import LikedSongs from "../pages/liked-songs";
+import LikedSongs, { getServerSideProps } from "../../pages/liked-songs";
+import { baseURL, server } from "../mocks/setupServer";
 
 jest.mock("next-auth/react", () => {
     return {
@@ -17,17 +20,76 @@ jest.mock("next-auth/react", () => {
                 },
             };
         },
+        getSession: jest
+            .fn()
+            .mockResolvedValueOnce({ accessToken: "accessToken" })
+            .mockResolvedValueOnce(false),
+        signIn: jest.fn(),
+    };
+});
+
+jest.mock("../../utils/testToken", () => {
+    return {
+        testToken: jest
+            .fn()
+            .mockReturnValueOnce(true)
+            .mockReturnValueOnce(false),
     };
 });
 
 describe("LikedSongs page", () => {
-    it("Should render LikedSongs page", () => {
+    it("Shouldn't be able to render the liked songs if data is not found.", async () => {
+        server.use(
+            rest.get(`${baseURL}me/tracks`, (req, res, ctx) => {
+                return res(ctx.status(400));
+            }),
+        );
+
         render(<LikedSongs accessToken="accessToken" />);
 
-        const Navigation = screen.getByText("Home");
-        const Search = screen.getByPlaceholderText("Search");
+        const title = screen.getByText("Liked Songs");
+        const name = screen.queryByText("Tuono");
 
-        expect(Navigation).toBeInTheDocument();
-        expect(Search).toBeInTheDocument();
+        expect(title).toBeInTheDocument();
+        expect(name).not.toBeInTheDocument();
+    });
+
+    it("Should render LikedSongs page with data correctly", async () => {
+        render(<LikedSongs accessToken="accessToken" />);
+
+        const title = screen.getByText("Liked Songs");
+        const name = await screen.findAllByText("Tuono");
+
+        expect(title).toBeInTheDocument();
+        expect(name[0]).toBeInTheDocument();
+    });
+
+    it("getServerSideProps should be able to return a default props if user is authenticated.", async () => {
+        const ctx = {};
+
+        const response = await getServerSideProps(
+            ctx as GetServerSidePropsContext,
+        );
+
+        expect(response).toEqual({
+            props: {
+                accessToken: "accessToken",
+            },
+        });
+    });
+
+    it("getServerSideProps should be able to return a redirect props if user isn't authenticated.", async () => {
+        const ctx = {};
+
+        const response = await getServerSideProps(
+            ctx as GetServerSidePropsContext,
+        );
+
+        expect(response).toEqual({
+            redirect: {
+                destination: "/",
+                permanent: false,
+            },
+        });
     });
 });
