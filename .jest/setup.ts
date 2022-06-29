783f066fb2ce20f2459049d4b998986529e52427
Wesley Westelley <wesleywestelley@gmail.com>
2022-06-29 16:59:01 -0300

    :wrench: chore: configure server to run fetch api in tests

--- .jest/setup.ts
+++ .jest/setup.ts
@@ -1,1 +1,10 @@
-import "@testing-library/jest-dom"
+import "@testing-library/jest-dom";
+import { server } from "../src/tests/mocks/setupServer";
+
+beforeAll(() =>
+    server.listen({
+        onUnhandledRequest: "error",
+    }),
+);
+afterAll(() => server.close());
+afterEach(() => server.resetHandlers());
