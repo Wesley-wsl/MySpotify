a71e666575159d47adab0429f9f650da794a0fd9
Wesley Westelley <wesleywestelley@gmail.com>
2022-06-29 23:08:18 -0300

    :test_tube: test: ensure that all information about artists is render

--- src/tests/pages/Artists.test.tsx
+++ src/tests/pages/Artists.test.tsx
@@ -1,6 +1,9 @@
 import { screen, render } from "@testing-library/react";
+import { rest } from "msw";
+import { GetServerSidePropsContext } from "next";
 
-import Artists from "../pages/artists";
+import Artists, { getServerSideProps } from "../../pages/artists";
+import { baseURL, server } from "../mocks/setupServer";
 
 jest.mock("next-auth/react", () => {
     return {
@@ -17,17 +20,70 @@ jest.mock("next-auth/react", () => {
                 },
             };
         },
+        getSession: jest
+            .fn()
+            .mockResolvedValueOnce({ accessToken: "accessToken" })
+            .mockResolvedValueOnce(false),
+        signIn: jest.fn(),
+    };
+});
+
+jest.mock("../../utils/testToken", () => {
+    return {
+        testToken: jest
+            .fn()
+            .mockReturnValueOnce(true)
+            .mockReturnValueOnce(false),
     };
 });
 
 describe("Artists page", () => {
-    it("Should render Artists page", () => {
+    it("Shouldn't be able to render the artists if data is not found.", async () => {
+        server.use(
+            rest.get(`${baseURL}me/following`, (req, res, ctx) => {
+                return res(ctx.status(400));
+            }),
+        );
+
         render(<Artists accessToken="accessToken" />);
 
-        const Navigation = screen.getByText("Home");
-        const Search = screen.getByPlaceholderText("Search");
+        const title = screen.queryByText("Artists that You Is Following");
+        expect(title).not.toBeInTheDocument();
+    });
 
-        expect(Navigation).toBeInTheDocument();
-        expect(Search).toBeInTheDocument();
+    it("Should render artists page with data correctly", async () => {
+        render(<Artists accessToken="accessToken" />);
+
+        const title = await screen.findByText("Artists that You Is Following");
+        expect(title).toBeInTheDocument();
+    });
+
+    it("getServerSideProps should be able to return a default props if user is authenticated.", async () => {
+        const ctx = {};
+
+        const response = await getServerSideProps(
+            ctx as GetServerSidePropsContext,
+        );
+
+        expect(response).toEqual({
+            props: {
+                accessToken: "accessToken",
+            },
+        });
+    });
+
+    it("getServerSideProps should be able to return a redirect props if user isn't authenticated.", async () => {
+        const ctx = {};
+
+        const response = await getServerSideProps(
+            ctx as GetServerSidePropsContext,
+        );
+
+        expect(response).toEqual({
+            redirect: {
+                destination: "/",
+                permanent: false,
+            },
+        });
     });
 });
