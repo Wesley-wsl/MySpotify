3faa52f6eb32ab5ca036caea1ac825f51ad8b8a1
Wesley Westelley <wesleywestelley@gmail.com>
2022-06-29 22:54:26 -0300

    :test_tube: test: ensure that BrowseSearch work correctly

--- src/tests/pages/BrowseSearch.test.tsx
+++ src/tests/pages/BrowseSearch.test.tsx
@@ -1,6 +1,9 @@
-import { screen, render } from "@testing-library/react";
+import { screen, render, waitFor } from "@testing-library/react";
+import { rest } from "msw";
+import { GetServerSidePropsContext } from "next";
 
-import Browse from "../pages/browse";
+import BrowseSearch, { getServerSideProps } from "../../pages/browse/[search]";
+import { baseURL, server } from "../mocks/setupServer";
 
 jest.mock("next-auth/react", () => {
     return {
@@ -17,15 +20,97 @@ jest.mock("next-auth/react", () => {
                 },
             };
         },
+        getSession: jest
+            .fn()
+            .mockResolvedValueOnce({ accessToken: "accessToken" })
+            .mockResolvedValueOnce(false),
+        signIn: jest.fn(),
+    };
+});
+
+jest.mock("../../utils/testToken", () => {
+    return {
+        testToken: jest
+            .fn()
+            .mockReturnValueOnce(true)
+            .mockReturnValueOnce(false),
+    };
+});
+
+jest.mock("next/router", () => {
+    return {
+        useRouter() {
+            return {
+                query: "any",
+            };
+        },
     };
 });
 
 describe("BrowseSearch page", () => {
-    it("Should render BrowseSearch page", () => {
-        render(<Browse />);
+    it("Shouldn't be able to render the list if data is not found.", async () => {
+        server.use(
+            rest.get(`${baseURL}search`, (req, res, ctx) => {
+                return res(ctx.status(400));
+            }),
+        );
 
-        const RecentlyPlayed = screen.getByPlaceholderText("Search");
+        render(<BrowseSearch accessToken="accessToken" />);
 
-        expect(RecentlyPlayed).toBeInTheDocument();
+        const artists = screen.queryByText("Artists");
+        const albums = screen.queryByText("Albums");
+        const playlists = screen.queryByText("Playlists");
+
+        expect(artists).not.toBeInTheDocument();
+        expect(albums).not.toBeInTheDocument();
+        expect(playlists).not.toBeInTheDocument();
+    });
+
+    it("Should render BrowseSearch page with data correctly", async () => {
+        render(<BrowseSearch accessToken="accessToken" />);
+
+        waitFor(async () => {
+            const artists = await screen.findByText("Artists");
+            expect(artists).toBeInTheDocument();
+        });
+
+        waitFor(async () => {
+            const albums = await screen.findByText("Albums");
+            expect(albums).toBeInTheDocument();
+        });
+
+        waitFor(async () => {
+            const playlists = screen.findByText("Playlists");
+            expect(playlists).toBeInTheDocument();
+        });
+    });
+
+    it("getServerSideProps should be able to return a default props if user is authenticated.", async () => {
+        const ctx = {};
+
+        const response = await getServerSideProps(
+            ctx as GetServerSidePropsContext,
+        );
+
+        expect(response).toEqual({
+            props: {
+                accessToken: "accessToken",
+            },
+        });
+    });
+
+    it("getServerSideProps should be able to return a redirect props if user isn't authenticated.", async () => {
+        const ctx = {};
+
+        const response = await getServerSideProps(
+            ctx as GetServerSidePropsContext,
+        );
+
+        expect(response).toEqual({
+            redirect: {
+                destination: "/",
+                permanent: false,
+            },
+        });
     });
 });
